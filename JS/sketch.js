308149df3f5697feb3c9afcc3e0f142de45bdd68
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-10-08 16:32:34 -0600

    message

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,16 +1,22 @@
+// Here, I am building an object, which I can pass using a POST request to the api.
+var apiObj = {
+    "appId":"530556bf",
+    "appKey":"e91e3c9087ca956da4e3b4328985830a",
+    "filters":{
+        "nf_protien":{
+            "item_type":2
+        }
+    }
+};
+// Now I'm changing the type to POST, the contentType to application/json, and passing in our apiObj using the JSON.stringify() method, which will convert our object to valid JSON.
 $(document).ready(function(){
     $.ajax({
-        type: "GET",
-        url: "https://api.nutritionix.com/v1_1/search" + "e91e3c9087ca956da4e3b4328985830a" + "530556bf",
-        "appId":"530556bf",
-        "appKey":"e91e3c9087ca956da4e3b4328985830a",
-        "filters":{
-          "not":{
-            "item_type":2
-    }
-  }
-    success: function(result){
-      console.log(result);
-    };
+        type: "POST",
+        contentType: "application/json",
+        url: "https://api.nutritionix.com/v1_1/search",
+        data: JSON.stringify(apiObj),
+        success: function(result) {
+            console.log(result);
+        }
     });
 });
