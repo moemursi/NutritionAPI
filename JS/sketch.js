58971663731478df2888cb50fc1a9b886f3016fa
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-10-08 16:22:52 -0600

    new stuff

--- JS/sketch.js
+++ JS/sketch.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
     $.ajax({
         type: "GET",
         url: "https://api.nutritionix.com/v1_1/search" + "e91e3c9087ca956da4e3b4328985830a" + "530556bf",
-        success: function(result){
-            console.log(result);
-        }
+        "appId":"530556bf",
+        "appKey":"e91e3c9087ca956da4e3b4328985830a",
+        "filters":{
+          "not":{
+            "item_type":2
+    }
+  }
+    success: function(result){
+      console.log(result);
+    };
     });
 });
